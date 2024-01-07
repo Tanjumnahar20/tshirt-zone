5cb14cebed2ac085c4504a9f29e087a8ca2ff6d5
Tanjum Nahar <tanjumnaharchandana@gmail.com>
2024-01-07 19:48:49 +0600

    toaster added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,23 +3,34 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
-import './Home.css'
+import './Home.css';
+import toast from 'react-hot-toast';
+
 
 const Home = () => {
     const tshirts = useLoaderData()
 
     // state declaration
-    const [cart,setCart] =useState([]);
-   
-    const handleAddToCart=tshirt=>{
-        const newCart =[...cart,tshirt];
-        setCart(newCart)
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        // handle buy not more than once
+        const exists = cart.find(ts => ts._id == tshirt._id)
+        if (exists) {
+            toast('you have already added')
+        }
+
+        else {
+
+            const newCart = [...cart, tshirt];
+            setCart(newCart)
+        }
     }
 
     // handle remove from cart
 
-    const handleRemove=id=>{
-        const remaining = cart.filter(ts=>ts._id!==id)
+    const handleRemove = id => {
+        const remaining = cart.filter(ts => ts._id !== id)
         setCart(remaining)
     }
 
@@ -27,17 +38,17 @@ const Home = () => {
         <div className='home-container'>
             <div className='t-shirts-container'>
                 {
-                    tshirts.map(tshirt =><Tshirt
-                    key={tshirt._id}
-                    tshirt={tshirt}
-                    handleAddToCart ={handleAddToCart}
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></Tshirt>)
                 }
             </div>
             <div className="cart-container">
                 <Cart
-                cart={cart}
-                handleRemove={handleRemove}
+                    cart={cart}
+                    handleRemove={handleRemove}
                 ></Cart>
             </div>
         </div>
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import  { Toaster } from 'react-hot-toast';
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Toaster   position="top-center"
+></Toaster>
         </div>
     );
 };
